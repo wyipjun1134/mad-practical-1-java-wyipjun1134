d16c6e2dd93e507e4d3647cfc2ef7a42f2a9dd7e
wyipjun1134 <59818032+wyipjun1134@users.noreply.github.com>
2020-04-26 17:22:05 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -9,8 +9,10 @@ public class Question3
      * 
      * Hint: in.nextInt() to read integer
      */
-     
+   
     Scanner in = new Scanner(System.in);
+    int integer = in.nextInt();
+    System.out.println(integer * integer);
     
   }
 }
