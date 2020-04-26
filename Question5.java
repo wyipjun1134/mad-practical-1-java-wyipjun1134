c63a8a40266cf924cd5bd63be29f3454466f809f
wyipjun1134 <59818032+wyipjun1134@users.noreply.github.com>
2020-04-26 18:51:47 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -26,7 +26,34 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
-    Scanner in = new Scanner(System.in);
-    
+   Scanner in = new Scanner(System.in);
+    int num = in.nextInt();
+    int count = 0;
+    int mode = 0;
+    ArrayList<Integer> mylist = new ArrayList<>();
+
+    while (mylist.size() < num)
+    {
+      int newnum = in.nextInt();
+      mylist.add(newnum);
+      for (int i = 0; i < mylist.size(); i++)
+      {
+        int c = 0;
+
+        for (int n = 0; n < mylist.size(); n++)
+        {
+          if (mylist.get(i) == mylist.get(n))
+          {
+            c += 1;
+            if (c-1 > count)
+            {
+              count = c;
+              mode = mylist.get(i);
+            }
+          }
+        }
+      }
+    }
+      System.out.println("\n" + mode);
   }
 }
