4361d8a0df69666aebe3af28fc3e8c622e944d83
wyipjun1134 <59818032+wyipjun1134@users.noreply.github.com>
2020-04-26 17:16:00 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -12,5 +12,8 @@ public class Question2
      
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
+    double weight = in.nextDouble();
+    
+    System.out.println(weight / (height * height));
   }
 }
