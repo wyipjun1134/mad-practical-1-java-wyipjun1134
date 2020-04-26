b032dccc4075f07aff78d730c8e91dc47f5ae78c
wyipjun1134 <59818032+wyipjun1134@users.noreply.github.com>
2020-04-26 17:34:24 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -16,8 +16,19 @@ public class Question4
      * 
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
-     
     Scanner in = new Scanner(System.in);
+    int base = in.nextInt();
+    int count = base;
+    for(int i = 0;i<= base;i++)
+    {
+      for(int n = 0; n <= count;n++)
+      {
+        System.out.print("*");
+        
+      }
+      count -= 1;
+      System.out.print("\n");
+    }
     
   }
 }
